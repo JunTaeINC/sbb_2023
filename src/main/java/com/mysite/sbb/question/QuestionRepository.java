7fd8f3761509e4310813ffedcd31cf6c3eb88eac
JunTaeINC <juntea3523@naver.com>
2023-03-21 15:48:54 +0900

    03-02

--- src/main/java/com/mysite/sbb/question/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/question/QuestionRepository.java
@@ -1,6 +1,10 @@
 package com.mysite.sbb.question;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 
 // Question Id Type -> Integer
@@ -8,4 +12,8 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubject(String subject);
 
     Question findBySubjectAndContent(String subject, String content);
+
+    List<Question> findBySubjectLike(String subject);
+
+    Page<Question> findAll(Pageable pageable);
 }
