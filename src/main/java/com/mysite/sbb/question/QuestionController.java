493fb8323421df6b31707390465e40aacab89b12
JunTaeINC <juntea3523@naver.com>
2023-03-27 14:53:13 +0900

    02-07

---
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -0,0 +1,27 @@
+package com.mysite.sbb.question;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+
+// final 이 붙은 속성을 포함하는 생성자를 자동으로 생성
+@RequiredArgsConstructor
+@Controller
+public class QuestionController {
+
+    private final QuestionRepository questionRepository;
+
+    @GetMapping("/question/list")
+
+
+    // Model 객체는 자바 클래스와 템플릿 간의 연결고리 역할
+    public String list(Model model) {
+        List<Question> questionList = this.questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
+        return "question_list";
+    }
+}
