f2a4521001b1d80962e0f833b6e39b8a02e53641
JunTaeINC <juntea3523@naver.com>
2023-03-27 15:41:09 +0900

    02-10

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
@@ -11,13 +13,17 @@ import java.util.List;
 // final 이 붙은 속성을 포함하는 생성자를 자동으로 생성
 @RequiredArgsConstructor
 @Controller
+@RequestMapping("/question")
 public class QuestionController {
 
     private final QuestionService questionService;
 
-    @GetMapping("/question/list")
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/question/list";
+    }
 
-
+    @GetMapping("/list")
     // Model 객체는 자바 클래스와 템플릿 간의 연결고리 역할
     public String list(Model model) {
         List<Question> questionList = this.questionService.getList();
@@ -25,8 +31,11 @@ public class QuestionController {
         return "question_list";
     }
 
-    @GetMapping("/")
-    public String root() {
-        return "redirect:/question/list";
+    @GetMapping(value = "/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
     }
+
 }
